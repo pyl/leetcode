47aac44e2b13b9a30d0d83803cfeb1c26ce33545
pyl <41081515+pyl@users.noreply.github.com>
2022-05-25 01:19:23 -0400

    Time: 271 ms (18.92%), Space: 14.4 MB (5.44%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        r = 0
+        chars = set()
+        res = 0
+        
+        while r < len(s):
+            
+            # process new info
+            
+            #check if sliding window is valid
+            if s[r] in chars:
+                #sliding window is not valid.
+                
+                #make it valid
+                while(s[r] in chars):
+                    chars.remove(s[l])
+                    l += 1
+                chars.add(s[r])
+            else:
+                chars.add(s[r])
+            
+            #now that the sliding window is valid, update information
+            print(l, r)
+            res = max(res, r - l + 1)
+            r += 1
+        return res
+            
