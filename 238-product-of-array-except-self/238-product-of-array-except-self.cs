c5c1447a8adc5fcc6e280fae26fcc0bbf907cc39
pyl <41081515+pyl@users.noreply.github.com>
2022-06-09 09:49:25 -0700

    Time: 198 ms (63.37%), Space: 49 MB (59.44%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cs
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cs
@@ -1,20 +1,21 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        
-                int[] res = new int[nums.Length];
+        int[] res = new int[nums.Length];
         int temp = 1;
+        
+        //get product from left
         for (int i = 0; i < nums.Length; i++) {
             res[i] = temp;
             temp *= nums[i];
         }
-        
         temp = 1;
         
+        
+        //get product from right
         for(int i = nums.Length - 1; i >= 0; i--) {
             res[i] *= temp;
             temp *= nums[i];
         }
-        
         return res;
     }
 }
