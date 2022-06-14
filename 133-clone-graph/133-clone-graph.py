6490d189223c1ff7d628ba36c0e71b33e2b4e53b
pyl <41081515+pyl@users.noreply.github.com>
2022-06-14 14:38:33 -0700

    Time: 47 ms (68.27%), Space: 14.3 MB (76.49%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        hm = dict()
+        def dfs(node):
+            if node == None:
+                return None
+
+            nn = Node(node.val)
+            hm[node] = nn
+            for x in node.neighbors:
+                if x in hm:
+                    nn.neighbors.append(hm[x])
+                else:
+                    nn.neighbors.append(dfs(x))
+            return nn
+        return dfs(node)
+    
+    # you start with one node. You create a node copy. You save references to all its neighbors. I
+    # Iterate through the original. For all neighbors in the original, add as neighbors to the corresponding node in the hashmap. 
+    
+    # How to deal with cycles? Have a set. Than way, 
+    
+    # create a hashmap from one node to another node
