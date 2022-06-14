ad6a5f9c1f4d9e6aea48d45819ce46428ac88767
pyl <41081515+pyl@users.noreply.github.com>
2022-06-14 14:53:44 -0700

    Time: 357 ms (5.04%), Space: 17.7 MB (23.38%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        visited = set()
+        
+        
+        
+        def si(row, col):
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or (row, col) in visited or grid[row][col] == 0:
+                return 0
+            else:
+                visited.add((row, col))
+                retval = 0
+                retval += 1 + si(row-1, col) + si(row+1, col) + si(row, col-1) + si(row, col + 1)
+                return retval
+        ret = 0
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 1 and grid[row][col] not in visited:
+                    ret = max(ret, si(row, col))
+        return ret
