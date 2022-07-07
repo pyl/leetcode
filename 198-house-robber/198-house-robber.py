fc0fb9915d289533ebb58abd7f67647cce5d2102
pyl <41081515+pyl@users.noreply.github.com>
2022-07-06 17:40:25 -0700

    Time: 65 ms (12.45%), Space: 14 MB (19.32%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        h1 = 0
+        h2 = 0
+        for i in range(len(nums)):
+            temp = max(nums[i] + h1, h2)
+            h1 = h2
+            h2 = temp
+        return h2
