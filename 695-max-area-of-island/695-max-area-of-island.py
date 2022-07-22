ab398d6e3a1baf468d93785986c946e3cbdc48e3
pyl <41081515+pyl@users.noreply.github.com>
2022-07-22 02:02:40 -0700

    Time: 157 ms (84.26%), Space: 18.2 MB (5.15%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -2,18 +2,24 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        visited = set()
         
-        
-        
-        def si(row, col):
-            if row < 0 or row >= ROWS or col < 0 or col >= COLS or (row, col) in visited or grid[row][col] == 0:
+        bigmax = 0
+        vs = set()
+        def dfs(i, j):
+            if i < 0 or i >= ROWS or j < 0 or j >= COLS or (i, j) in vs or grid[i][j] == 0:
                 return 0
-            else:
-                visited.add((row, col))
-                return 1 + si(row-1, col) + si(row+1, col) + si(row, col-1) + si(row, col + 1)
-        ret = 0
-        for row in range(ROWS):
-            for col in range(COLS):
-                ret = max(ret, si(row, col))
-        return ret
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            maxx = 1
+            vs.add((i,j))
+            for y, x in directions:
+                maxx += dfs(i + y, x + j)
+            return maxx
+        for y in range(ROWS):
+            for x in range(COLS):
+                if (y, x) not in vs:
+                    bigmax = max(bigmax, dfs(y, x))
+        return bigmax
+            
+                
+                    
+                
