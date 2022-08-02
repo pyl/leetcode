3e1de6b3ab575e7ecaa4ef2b8d2d10fa972a35be
pyl <41081515+pyl@users.noreply.github.com>
2022-08-01 22:21:22 -0700

    Time: 378 ms (48.23%), Space: 15.6 MB (22.84%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,5 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # how to do binary search correctly?
+        # I i were to insert target into nums, what index would I insert it in?
         l = 0
         r = len(nums)
         while l < r:
@@ -10,4 +12,5 @@ class Solution:
                 l = m + 1
             else:
                 r = m
-        return -1   
+        return -1
+    
