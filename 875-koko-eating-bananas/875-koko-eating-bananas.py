6713a4b2f8657dcfbce2fe495e36785bd3436332
pyl <41081515+pyl@users.noreply.github.com>
2022-08-04 19:32:41 -0700

    Time: 711 ms (50.22%), Space: 15.5 MB (75.57%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -2,7 +2,7 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         # choose h. 
         l = 1
-        r = max(piles) + 1
+        r = max(piles)
         while l < r:
             speed = (l + r) // 2
             hours = 0
