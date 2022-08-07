847a76d82c2a88e9e11c3b52bcc8e41c75d7c8d0
pyl <41081515+pyl@users.noreply.github.com>
2022-08-07 11:38:34 -0700

    Time: 68 ms (45.63%), Space: 14.3 MB (56.29%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -2,22 +2,22 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums)
+        
+        #if your in nonbroken side
         while l < r:
             m = (l + r) // 2
             if nums[m] == target:
                 return m
             if nums[l] < nums[m]:
-                # left portion
-                if nums[m] < target or target < nums[l]:
+                if target > nums[m] or target < nums[l]:
                     l = m + 1
                 else:
                     r = m
             else:
-                # left portion
-                if nums[m] > target or target > nums[r-1]:
+                if target < nums[m] or target > nums[r-1]:
                     r = m
                 else:
                     l = m + 1
-        return -1
-        
+        return - 1
+                
             
