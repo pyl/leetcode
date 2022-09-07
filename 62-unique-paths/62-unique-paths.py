679149ece5feac80c4e8fa626ccdc05e0abe4984
pyl <41081515+pyl@users.noreply.github.com>
2022-09-07 13:58:12 -0400

    Time: 70 ms (7.16%), Space: 14 MB (33.03%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -4,5 +4,6 @@ class Solution:
         dp[1][1] = 1
         for y in range(1, m + 1):
             for x in range(1, n + 1):
+                print(dp[y][x])
                 dp[y][x] = dp[y-1][x] + dp[y][x-1]
         return dp[m][n]
