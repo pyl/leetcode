645e503db4e8aa9ffa0aee0abac41be0d684ef73
pyl <41081515+pyl@users.noreply.github.com>
2022-10-29 04:04:31 -0400

    Time: 165 ms (5.10%), Space: 13.9 MB (83.45%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -5,7 +5,7 @@ class Solution:
             if i == len(s) and dots == 4:
                 res.append(sofar[:-1])
                 return
-            if dots > 4:
+            if i > len(s):
                 return
             for j in range(i, min(i + 3, len(s))):
                 candidate = s[i:j + 1]
@@ -15,8 +15,12 @@ class Solution:
                 # if length 1, 0 is fine to be included
                 if len(candidate) == 1:
                     backtracking(j + 1, dots + 1, sofar + candidate + ".")
+                
+                # if length 2, make sure it doesnt start with 0
                 if len(candidate) == 2 and candidate[0] != "0":
                     backtracking(j + 1, dots + 1, sofar + candidate + ".")
+                    
+                # if length 3, make sure it's less than 255
                 if len(candidate) == 3 and candidate[0] != "0" and int(candidate) <= 255:
                     backtracking(j + 1, dots + 1, sofar + candidate + ".")
         backtracking(0, 0, "")
